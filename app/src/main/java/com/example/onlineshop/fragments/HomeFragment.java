48660d1ba01194e73b78f82337fb042a89548d09
Pritom <pritomsarkar300@gmail.com>
2022-05-10 15:41:22 +0600

    Catogry pic show form firebase

--- app/src/main/java/com/example/onlineshop/fragments/HomeFragment.java
+++ app/src/main/java/com/example/onlineshop/fragments/HomeFragment.java
@@ -2,7 +2,10 @@ package com.example.onlineshop.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,13 @@ import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.onlineshop.R;
+import com.example.onlineshop.adapters.CategoryAdapter;
+import com.example.onlineshop.models.CategoryModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +29,14 @@ import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    RecyclerView catRecyclerview;
 
+    //Category Recyclerview
+    CategoryAdapter categoryAdapter;
+    List<CategoryModel> categoryModelList;
+
+    //FireStore
+    FirebaseFirestore db;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -33,6 +50,10 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        catRecyclerview = root.findViewById(R.id.rec_category);
+
+        db = FirebaseFirestore.getInstance();
+
         //image slider
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
         List<SlideModel> slideModels = new ArrayList<>();
@@ -42,6 +63,33 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.banner3,"70% OFF", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+        //Category
+        catRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryModelList = new ArrayList<>();
+        categoryAdapter = new CategoryAdapter(getContext(),categoryModelList);
+        catRecyclerview.setAdapter(categoryAdapter);
+
+
+        //Data read from firebase
+        db.collection("Category")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+
+                                CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                                categoryModelList.add(categoryModel);
+                                categoryAdapter.notifyDataSetChanged();
+
+                            }
+                        } else {
+
+                        }
+                    }
+                });
 
         return root;
     }
